c177b8bf9b8b42136d56ae61ae870a69c8d9e1ec
Ross Hendrickson <savorywatt@ross-dev.(none)>
2014-02-09 21:22:53 -0600

    Add preprocessor

---
+++ src/octoprint/util/preprocessor.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+
+__author__ = "Ross Hendrickson <ross.hendrickson@gmail.com>"
+__license__ = 'GNU Affero General Public License http://www.gnu.org/licenses/agpl.html'
+
+
+import os
+
+
+def rename_contiguous_images(prefix, postfix, folder_path):
+    """Ensures the files in the given folder are numbered
+    in the correct order.
+
+    :param prefix: :class: `str`
+    :param postfix: :class: `str`
+    :param folder_path: :class: `str`
+    """
+
+    # Iterate over all files in the folder
+    # retrieve names
+    # sort file_names
+    # rename them so their numbers are contigious
+
+
